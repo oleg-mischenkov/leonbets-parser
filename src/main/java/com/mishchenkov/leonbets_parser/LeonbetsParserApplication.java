4cbab69fb1892540c3eb9a1459211acfe5304493
Oleg Mischenkov <mischenkov.oleg@gmail.com>
2025-02-06 23:10:32 +0100

    the init point

---
+++ src/main/java/com/mishchenkov/leonbets_parser/LeonbetsParserApplication.java
@@ -0,0 +1,13 @@
+package com.mishchenkov.leonbets_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonbetsParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonbetsParserApplication.class, args);
+	}
+
+}
